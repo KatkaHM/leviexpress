bfc51d1d83dbafecbd116184f003e2b0f677c063
Katka Martínková <martinkova.katie@gmail.com>
2021-05-22 12:47:12 +0200

    journey picker logic

--- src/Home/JourneyPicker/index.jsx
+++ src/Home/JourneyPicker/index.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import map from './img/map.svg';
 
 export const JourneyPicker = () => {
+  const [fromCity, setFromCity] = useState('');
+  const [toCity, setToCity] = useState('');
+  const [date, setDate] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Odesilam nekam neco', fromCity, toCity, date);
+  };
+
   return (
     <div className="journey-picker container">
       <h2 className="journey-picker__head">Kam chcete jet?</h2>
       <div className="journey-picker__body">
-        <form className="journey-picker__form">
+        <form onSubmit={handleSubmit} className="journey-picker__form">
           <label>
             <div className="journey-picker__label">Odkud:</div>
-            <select>
+            <select
+              value={fromCity}
+              onChange={(e) => setFromCity(e.target.value)}
+            >
               <option value="">Vyberte</option>
               <option value="Mesto1">Město 1</option>
               <option value="Mesto2">Město 2</option>
@@ -20,7 +32,7 @@ export const JourneyPicker = () => {
           </label>
           <label>
             <div className="journey-picker__label">Kam:</div>
-            <select>
+            <select value={toCity} onChange={(e) => setToCity(e.target.value)}>
               <option value="">Vyberte</option>
               <option value="Mesto1">Město 1</option>
               <option value="Mesto2">Město 2</option>
@@ -30,7 +42,7 @@ export const JourneyPicker = () => {
           </label>
           <label>
             <div className="journey-picker__label">Datum:</div>
-            <select>
+            <select value={date} onChange={(e) => setDate(e.target.value)}>
               <option value="">Vyberte</option>
               <option>20.05.2021</option>
               <option>21.05.2021</option>
